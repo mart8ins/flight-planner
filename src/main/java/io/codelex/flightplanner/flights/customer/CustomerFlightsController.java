ba41b3cdf3446d47b87878656e0b92366f7a13c1
mart8ins <mart8ins@gmail.com>
2023-07-04 13:35:09 +0300

    connected controllers-services-repository

--- src/main/java/io/codelex/flightplanner/flights/customer/CustomerFlightsController.java
+++ src/main/java/io/codelex/flightplanner/flights/customer/CustomerFlightsController.java
@@ -2,22 +2,31 @@ package io.codelex.flightplanner.flights.customer;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/")
 public class CustomerFlightsController {
 
+    private CustomerFlightsService customerFlightsService;
+
+    public CustomerFlightsController(CustomerFlightsService customerFlightsService) {
+        this.customerFlightsService = customerFlightsService;
+    }
+
+
     @GetMapping("airports") // receives query parameter
-    public String searchAirport(@RequestParam String airport){
-        return "Airport search query "+ airport;
+    public List<String> searchAirport(@RequestParam String airport){
+        return customerFlightsService.getAirports(airport);
     }
 
     @GetMapping("flights/{flightId}") // receives path variable
     public String searchFlightById(@PathVariable String flightId){
-        return "Searched airport with id "+ flightId;
+        return "Searched airport with id "+ customerFlightsService.getFlightById(flightId);
     }
 
     @PostMapping("flights/search") // receives body/object
     public String searchFlights(@RequestBody String flights){
-        return "Searched flights with body, temporarily with string "+ flights;
+        return "Searched flights with body, temporarily with string "+ customerFlightsService.searchFlights(flights);
     }
 }
--- src/main/java/io/codelex/flightplanner/flights/customer/CustomerFlightsService.java
+++ src/main/java/io/codelex/flightplanner/flights/customer/CustomerFlightsService.java
@@ -1,4 +1,28 @@
 package io.codelex.flightplanner.flights.customer;
 
+import io.codelex.flightplanner.flights.FlightsRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CustomerFlightsService {
+
+    private FlightsRepository flightsRepository;
+
+    public CustomerFlightsService(FlightsRepository flightsRepository) {
+        this.flightsRepository = flightsRepository;
+    }
+
+    public List<String> getAirports(String query){
+        return flightsRepository.getAirports(query);
+    }
+
+    public String getFlightById(String flightId) {
+        return flightsRepository.getFlightById(flightId);
+    }
+
+    public String searchFlights(String object) {
+        return flightsRepository.searchFlights(object);
+    }
 }
