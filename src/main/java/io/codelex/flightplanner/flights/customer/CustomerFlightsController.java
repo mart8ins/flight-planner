fc272986c7c7920955647ce9121340b2fe27ebdf
mart8ins <mart8ins@gmail.com>
2023-07-06 15:13:13 +0300

    Implemented logic for customer to pass test for finding flight by id and not find anything when there is non

--- src/main/java/io/codelex/flightplanner/flights/customer/CustomerFlightsController.java
+++ src/main/java/io/codelex/flightplanner/flights/customer/CustomerFlightsController.java
@@ -1,19 +1,14 @@
 package io.codelex.flightplanner.flights.customer;
 
 import io.codelex.flightplanner.flights.admin.domain.Airport;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
+import io.codelex.flightplanner.flights.admin.response.AddFlightResponse;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/")
 public class CustomerFlightsController {
-
-    Logger logger = LoggerFactory.getLogger(CustomerFlightsController.class);
 
     private CustomerFlightsService customerFlightsService;
 
@@ -22,14 +17,14 @@ public class CustomerFlightsController {
     }
 
 
-    @GetMapping("airports") // receives query parameter
+    @GetMapping("airports")
     public List<Airport> searchAirport(@RequestParam String search){
         return customerFlightsService.searchAirport(search);
     }
 
-    @GetMapping("flights/{flightId}") // receives path variable
-    public String searchFlightById(@PathVariable String flightId){
-        return "Searched airport with id "+ customerFlightsService.getFlightById(flightId);
+    @GetMapping("flights/{flightId}")
+    public AddFlightResponse getFlightById(@PathVariable String flightId){
+        return customerFlightsService.getFlightById(flightId);
     }
 
     @PostMapping("flights/search") // receives body/object
--- src/main/java/io/codelex/flightplanner/flights/customer/CustomerFlightsService.java
+++ src/main/java/io/codelex/flightplanner/flights/customer/CustomerFlightsService.java
@@ -2,12 +2,18 @@ package io.codelex.flightplanner.flights.customer;
 
 import io.codelex.flightplanner.flights.FlightsRepository;
 import io.codelex.flightplanner.flights.admin.domain.Airport;
+import io.codelex.flightplanner.flights.admin.domain.Flight;
+import io.codelex.flightplanner.flights.admin.response.AddFlightResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CustomerFlightsService {
+
+    Logger logger = LoggerFactory.getLogger(CustomerFlightsService.class);
 
     private FlightsRepository flightsRepository;
 
@@ -19,9 +25,11 @@ public class CustomerFlightsService {
         return flightsRepository.searchAirport(airportSearchQuery);
     }
 
-    public String getFlightById(String flightId) {
-        return "";
-//        return flightsRepository.getFlightById(flightId);
+    public AddFlightResponse getFlightById(String flightId) {
+        Flight flightFromDatabase = flightsRepository.getFlightById(flightId);
+        logger.error("flightFromDatabase ir : " + flightFromDatabase);
+        return new AddFlightResponse(flightFromDatabase.getFrom(), flightFromDatabase.getTo(), flightFromDatabase.getCarrier(),
+                flightFromDatabase.getDepartureTime(), flightFromDatabase.getArrivalTime(), flightFromDatabase.getId());
     }
 
     public String searchFlights(String object) {
