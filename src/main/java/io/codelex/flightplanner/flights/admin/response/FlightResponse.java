6520750448f5da5798ed8952e8f8f9a5ac6b2a09
mart8ins <mart8ins@gmail.com>
2023-07-07 09:57:21 +0300

    removed class extendings

--- src/main/java/io/codelex/flightplanner/flights/admin/response/FlightResponse.java
+++ src/main/java/io/codelex/flightplanner/flights/admin/response/FlightResponse.java
@@ -1,17 +1,70 @@
 package io.codelex.flightplanner.flights.admin.response;
 
-import io.codelex.flightplanner.flights.admin.request.FlightRequest;
 import io.codelex.flightplanner.flights.admin.domain.Airport;
 
 import java.util.Objects;
 
-public class FlightResponse extends FlightRequest {
+public class FlightResponse {
+
+    private Airport from;
+
+    private Airport to;
+
+    private String carrier;
+
+    private String departureTime;
+
+    private String arrivalTime;
 
     private int id;
 
     public FlightResponse(Airport from, Airport to, String carrier, String departureTime, String arrivalTime, int id) {
-        super(from, to, carrier, departureTime, arrivalTime);
+        this.from = from;
+        this.to = to;
+        this.carrier = carrier;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
         this.id = id;
+    }
+
+    public Airport getFrom() {
+        return from;
+    }
+
+    public void setFrom(Airport from) {
+        this.from = from;
+    }
+
+    public Airport getTo() {
+        return to;
+    }
+
+    public void setTo(Airport to) {
+        this.to = to;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
     }
 
     public int getId() {
@@ -24,9 +77,14 @@ public class FlightResponse extends FlightRequest {
 
     @Override
     public String toString() {
-        return "AddFlightResponse{" +
-                "id='" + id + '\'' +
-                "} " + super.toString();
+        return "FlightResponse{" +
+                "from=" + from +
+                ", to=" + to +
+                ", carrier='" + carrier + '\'' +
+                ", departureTime='" + departureTime + '\'' +
+                ", arrivalTime='" + arrivalTime + '\'' +
+                ", id=" + id +
+                '}';
     }
 
     @Override
@@ -34,11 +92,13 @@ public class FlightResponse extends FlightRequest {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         FlightResponse that = (FlightResponse) o;
-        return id == that.id;
+        return id == that.id && Objects.equals(from, that.from) && Objects.equals(to, that.to) && Objects.equals(carrier, that.carrier) && Objects.equals(departureTime, that.departureTime) && Objects.equals(arrivalTime, that.arrivalTime);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(from, to, carrier, departureTime, arrivalTime, id);
     }
+
+
 }
