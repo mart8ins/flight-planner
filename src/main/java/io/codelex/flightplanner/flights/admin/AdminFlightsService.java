48f8249d8a9c22404add9f4af3c08ec6e2ce5815
mart8ins <mart8ins@gmail.com>
2023-07-07 11:52:30 +0300

    created HandleDatesFormater and changed applications date handling

--- src/main/java/io/codelex/flightplanner/flights/admin/AdminFlightsService.java
+++ src/main/java/io/codelex/flightplanner/flights/admin/AdminFlightsService.java
@@ -4,7 +4,10 @@ import io.codelex.flightplanner.flights.FlightsRepository;
 import io.codelex.flightplanner.flights.admin.domain.Flight;
 import io.codelex.flightplanner.flights.admin.request.FlightRequest;
 import io.codelex.flightplanner.flights.admin.response.FlightResponse;
+import io.codelex.flightplanner.flights.utils.HandleDatesFormatter;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -20,18 +23,29 @@ public class AdminFlightsService {
 
     public FlightResponse getFlightById(String flightId) {
         Flight flightFromDatabase = flightsRepository.getFlightById(flightId);
+
+        String departureDateTime = HandleDatesFormatter.formatLocalDateTimeToString(flightFromDatabase.getDepartureTime());
+        String arrivalDateTime = HandleDatesFormatter.formatLocalDateTimeToString(flightFromDatabase.getArrivalTime());
+
         return new FlightResponse(flightFromDatabase.getFrom(), flightFromDatabase.getTo(), flightFromDatabase.getCarrier(),
-                flightFromDatabase.getDepartureTime(), flightFromDatabase.getArrivalTime(), flightFromDatabase.getId());
+                departureDateTime, arrivalDateTime, flightFromDatabase.getId());
     }
 
     public FlightResponse saveFlight(FlightRequest flightRequest) {
         List<Flight> flights = flightsRepository.getFlights();
-        adminValidationsService.validateRequest(flights, flightRequest);
+
+        LocalDateTime departureDateTime = HandleDatesFormatter.formatStringToDateTime(flightRequest.getDepartureTime());
+        LocalDateTime arrivalDateTime = HandleDatesFormatter.formatStringToDateTime(flightRequest.getArrivalTime());
+
+        adminValidationsService.validateRequest(flights, flightRequest, departureDateTime, arrivalDateTime);
 
         int lastId = flights.stream().mapToInt(fl -> fl.getId()).max().orElse(0);
+
         Flight flightToSave = new Flight(flightRequest.getFrom(), flightRequest.getTo(),
-                flightRequest.getCarrier(), flightRequest.getDepartureTime(), flightRequest.getArrivalTime(), lastId + 1);
+                flightRequest.getCarrier(), departureDateTime, arrivalDateTime, lastId + 1);
+
         flightsRepository.addAirports(flightToSave);
+
         return flightsRepository.saveFlight(flightToSave);
     }
 
