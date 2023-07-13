22035d333283421a202007fd6936178d93e1fb17
mart8ins <mart8ins@gmail.com>
2023-07-13 10:19:32 +0300

    added unit tests for CustomerFligtService searchAirports method

--- src/test/java/io/codelex/flightplanner/flights/customer/CustomerFlightsServiceTest.java
+++ src/test/java/io/codelex/flightplanner/flights/customer/CustomerFlightsServiceTest.java
@@ -17,6 +17,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 class CustomerFlightsServiceTest {
@@ -56,4 +57,67 @@ class CustomerFlightsServiceTest {
         Assertions.assertEquals("Estonia", actualFlight.getTo().getCountry());
         Assertions.assertEquals("2023-06-02", actualFlight.getDepartureTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
     }
+
+    @Test
+    @DisplayName("Searched by half of country name returns one result")
+    void searchAirports1(){
+        String searchQuery = "Lat";
+
+        Airport airport1 = new Airport("Latvia", "Riga", "RIX");
+        List<Airport> expextedAirportList = List.of(airport1);
+
+        Mockito.when(flightsRepository.searchAirport(searchQuery)).thenReturn(expextedAirportList);
+
+        List<Airport> actualAirportList = customerFlightsService.searchAirport(searchQuery);
+
+        Assertions.assertEquals(1, actualAirportList.size());
+        Assertions.assertEquals("Latvia", actualAirportList.get(0).getCountry());
+        Assertions.assertEquals("Riga", actualAirportList.get(0).getCity());
+        Assertions.assertEquals("RIX", actualAirportList.get(0).getAirport());
+    }
+
+    @Test
+    @DisplayName("Searched by half of countries name returns two results")
+    void searchAirports2(){
+        String searchQuery = "Lat";
+
+        Airport airport1 = new Airport("Latvia", "Riga", "RIX");
+        Airport airport2 = new Airport("Latunia", "Oaua", "BIX");
+        List<Airport> expextedAirportList = List.of(airport1, airport2);
+
+        Mockito.when(flightsRepository.searchAirport(searchQuery)).thenReturn(expextedAirportList);
+
+        List<Airport> actualAirportList = customerFlightsService.searchAirport(searchQuery);
+
+        Assertions.assertEquals(2, actualAirportList.size());
+        Assertions.assertEquals("Latvia", actualAirportList.get(0).getCountry());
+        Assertions.assertEquals("Latunia", actualAirportList.get(1).getCountry());
+        Assertions.assertEquals("Riga", actualAirportList.get(0).getCity());
+        Assertions.assertEquals("Oaua", actualAirportList.get(1).getCity());
+        Assertions.assertEquals("RIX", actualAirportList.get(0).getAirport());
+        Assertions.assertEquals("BIX", actualAirportList.get(1).getAirport());
+    }
+
+    @Test
+    @DisplayName("Searched by part of airport name, returns more results")
+    void searchAirports3(){
+        String searchQuery = "IX";
+
+        Airport airport1 = new Airport("Latvia", "Riga", "RIX");
+        Airport airport2 = new Airport("Latunia", "Oaua", "BIX");
+        List<Airport> expextedAirportList = List.of(airport1, airport2);
+
+        Mockito.when(flightsRepository.searchAirport(searchQuery)).thenReturn(expextedAirportList);
+
+        List<Airport> actualAirportList = customerFlightsService.searchAirport(searchQuery);
+
+        Assertions.assertEquals(2, actualAirportList.size());
+        Assertions.assertEquals("Latvia", actualAirportList.get(0).getCountry());
+        Assertions.assertEquals("Latunia", actualAirportList.get(1).getCountry());
+        Assertions.assertEquals("Riga", actualAirportList.get(0).getCity());
+        Assertions.assertEquals("Oaua", actualAirportList.get(1).getCity());
+        Assertions.assertEquals("RIX", actualAirportList.get(0).getAirport());
+        Assertions.assertEquals("BIX", actualAirportList.get(1).getAirport());
+    }
+
 }
